f7f173f80ce07b47315771163d601d29f3f0d4bf
Hae-Riri <ehl3288@naver.com>
2022-01-10 11:37:20 +0900

    feat : 입력값 유효성 검사 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,29 @@ public class Application {
         strike = 0;
     }
 
+    private static boolean checkInputValidation() {
+        return !hasOverLength() && !hasSameNumber();
+    }
+
+    public static boolean hasOverLength() {
+        return input.length() > 3;
+    }
+
+    public static boolean hasSameNumber() {
+        int[] arr = new int[10]; //1-9
+
+        for(int i = 0; i < input.length(); i++) {
+            int idx = input.charAt(i) - '0';
+            arr[idx]++;
+        }
+
+        for(int i = 1; i < 10; i++){
+            if(arr[i] >= 2) return true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
     }
