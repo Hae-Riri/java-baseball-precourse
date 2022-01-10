337f05561e48d03436ddb31f85da5240fece6fa5
Hae-Riri <ehl3288@naver.com>
2022-01-10 11:17:39 +0900

    feat : 답안 초기화 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,16 @@
 package baseball;
 
 public class Application {
+
+    static int ball = 0, strike = 0;
+    static StringBuilder number = new StringBuilder(), input = new StringBuilder();
+    static String again;
+
+    public static void answerInit() {
+        ball = 0;
+        strike = 0;
+    }
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
     }
